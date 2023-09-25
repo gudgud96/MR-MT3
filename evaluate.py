37bfebb8a3d165a2184258c36df2943f07a8a48c
gudgud96 <tanh0207@e.ntu.edu.sg>
2023-09-25 22:09:12 +0800

    [feat] add evaluation script

--- evaluate.py
+++ evaluate.py
@@ -52,173 +52,14 @@ def compute_transcription_metrics(ref_mid, est_mid):
   }
 
 
-def mt3_program_aware_note_scores_v2(
-    ref_mid, est_mid, granularity_type, offset=False
-):
-    res = dict()
-    ref_ns = note_seq.midi_file_to_note_sequence(ref_mid)
-    est_ns = note_seq.midi_file_to_note_sequence(est_mid)
-    
-    def remove_drums(ns):
-      ns_drumless = note_seq.NoteSequence()
-      ns_drumless.CopyFrom(ns)
-    #   del ns_drumless.notes[:]
-    #   ns_drumless.notes.extend([note for note in ns.notes if not note.is_drum])
-      return ns_drumless
-
-    est_ns_drumless = remove_drums(est_ns)
-    ref_ns_drumless = remove_drums(ref_ns)
-
-    # Whether or not there are separate tracks, compute metrics for the full
-    # NoteSequence minus drums.
-    est_tracks = [est_ns_drumless]
-    ref_tracks = [ref_ns_drumless]
-    use_track_offsets = [False]
-    use_track_velocities = [False]
-    track_instrument_names = ['']
-
-    for est_ns, ref_ns, use_offsets, use_velocities, instrument_name in zip(
-        est_tracks, ref_tracks, use_track_offsets, use_track_velocities,
-        track_instrument_names):
-
-      est_intervals, est_pitches, est_velocities = (
-          note_seq.sequences_lib.sequence_to_valued_intervals(est_ns))
-
-      ref_intervals, ref_pitches, ref_velocities = (
-          note_seq.sequences_lib.sequence_to_valued_intervals(ref_ns))
-
-      # Precision / recall / F1 using onsets (and pitches) only.
-      precision, recall, f_measure, avg_overlap_ratio = (
-          mir_eval.transcription.precision_recall_f1_overlap(
-              ref_intervals=ref_intervals,
-              ref_pitches=ref_pitches,
-              est_intervals=est_intervals,
-              est_pitches=est_pitches,
-              offset_ratio=None))
-      res['Onset precision'] = precision
-      res['Onset recall'] = recall
-      res['Onset F1'] = f_measure
-
-    program_map_fn = vocabularies.PROGRAM_GRANULARITIES[
-      granularity_type].program_map_fn
-
-    ref_ns = copy.deepcopy(ref_ns)
-    for note in ref_ns.notes:
-        if not note.is_drum:
-            note.program = program_map_fn(note.program)
-
-    est_ns = copy.deepcopy(est_ns)
-    for note in est_ns.notes:
-        if not note.is_drum:
-            note.program = program_map_fn(note.program)
-
-    # print(granularity_type, 'ref', set((note.program, note.is_drum) for note in ref_ns.notes))
-    # print(granularity_type, 'est', set((note.program, note.is_drum) for note in est_ns.notes))
-    program_and_is_drum_tuples = (
-        set((note.program, note.is_drum) for note in ref_ns.notes) |
-        set((note.program, note.is_drum) for note in est_ns.notes)
-    )
-    # print(granularity_type, 'program', program_and_is_drum_tuples)
-
-    drum_precision_sum = 0.0
-    drum_precision_count = 0
-    drum_recall_sum = 0.0
-    drum_recall_count = 0
-
-    nondrum_precision_sum = 0.0
-    nondrum_precision_count = 0
-    nondrum_recall_sum = 0.0
-    nondrum_recall_count = 0
-
-    program_f1 = {}
-    for program, is_drum in program_and_is_drum_tuples:
-        est_track = note_sequences.extract_track(est_ns, program, is_drum)
-        ref_track = note_sequences.extract_track(ref_ns, program, is_drum)
-
-        est_intervals, est_pitches, unused_est_velocities = (
-            note_seq.sequences_lib.sequence_to_valued_intervals(est_track))
-        ref_intervals, ref_pitches, unused_ref_velocities = (
-            note_seq.sequences_lib.sequence_to_valued_intervals(ref_track))
-
-        args = {
-            'ref_intervals': ref_intervals, 'ref_pitches': ref_pitches,
-            'est_intervals': est_intervals, 'est_pitches': est_pitches
-        }
-        if offset:
-            if is_drum:
-                args['offset_ratio'] = None
-        else:
-            args['offset_ratio'] = None
-            
-
-        precision, recall, f_measure, unused_avg_overlap_ratio = (
-            mir_eval.transcription.precision_recall_f1_overlap(**args))
-        
-        if granularity_type == "midi_class" and not is_drum:
-            program_f1[program] = f_measure
-        
-        # print('precision', precision)
-        # print('recall', recall)
-        # print("=====")
-
-        if is_drum:
-            drum_precision_sum += precision * len(est_intervals)
-            drum_precision_count += len(est_intervals)
-            drum_recall_sum += recall * len(ref_intervals)
-            drum_recall_count += len(ref_intervals)
-        else:
-            nondrum_precision_sum += precision * len(est_intervals)
-            nondrum_precision_count += len(est_intervals)
-            nondrum_recall_sum += recall * len(ref_intervals)
-            nondrum_recall_count += len(ref_intervals)
-
-    precision_sum = drum_precision_sum + nondrum_precision_sum
-    precision_count = drum_precision_count + nondrum_precision_count
-    recall_sum = drum_recall_sum + nondrum_recall_sum
-    recall_count = drum_recall_count + nondrum_recall_count
-
-    precision = (precision_sum / precision_count) if precision_count else 0
-    recall = (recall_sum / recall_count) if recall_count else 0
-    f_measure = mir_eval.util.f_measure(precision, recall)
-
-    drum_precision = ((drum_precision_sum / drum_precision_count)
-                        if drum_precision_count else 0)
-    drum_recall = ((drum_recall_sum / drum_recall_count)
-                    if drum_recall_count else 0)
-    drum_f_measure = mir_eval.util.f_measure(drum_precision, drum_recall)
-
-    nondrum_precision = ((nondrum_precision_sum / nondrum_precision_count)
-                        if nondrum_precision_count else 0)
-    nondrum_recall = ((nondrum_recall_sum / nondrum_recall_count)
-                        if nondrum_recall_count else 0)
-    nondrum_f_measure = mir_eval.util.f_measure(nondrum_precision, nondrum_recall)
-
-    
-    # print("v2", program_f1)
-    # res.update({
-    #     f'Onset + offset + program precision ({granularity_type})': precision,
-    #     f'Onset + offset + program recall ({granularity_type})': recall,
-    #     f'Onset + offset + program F1 ({granularity_type})': f_measure,
-    #     f'Drum onset precision ({granularity_type})': drum_precision,
-    #     f'Drum onset recall ({granularity_type})': drum_recall,
-    #     f'Drum onset F1 ({granularity_type})': drum_f_measure,
-    #     f'Nondrum onset + offset + program precision ({granularity_type})':
-    #         nondrum_precision,
-    #     f'Nondrum onset + offset + program recall ({granularity_type})':
-    #         nondrum_recall,
-    #     f'Nondrum onset + offset + program F1 ({granularity_type})':
-    #         nondrum_f_measure
-    # })
-    res.update({
-        "F1 by program": program_f1
-    })
-
-    return res
-
-
 def mt3_program_aware_note_scores(
     fname1, fname2, granularity_type
 ):
+    """
+    Edited version of MT3's program aware precision/recall/F1 score.
+    We follow Perceiver's evaluation approach which takes only onset and program into account.
+    Using MIDIs transcribed from MT3, we managed to get similar results as Perceiver, which is 0.75 for onset F1.
+    """
     ref_mid = pretty_midi.PrettyMIDI(fname1)
     est_mid = pretty_midi.PrettyMIDI(fname2)
 
@@ -226,24 +67,26 @@ def mt3_program_aware_note_scores(
     ref_ns = note_seq.midi_file_to_note_sequence(fname1)
     est_ns = note_seq.midi_file_to_note_sequence(fname2)
     
-    def remove_drums(ns):
-      ns_drumless = note_seq.NoteSequence()
-      ns_drumless.CopyFrom(ns)
+    # NOTE: We don't need to remove drums and process separately as in MT3
+    # as we consider onset only for all instruments.
+    # def remove_drums(ns):
+    #   ns_drumless = note_seq.NoteSequence()
+    #   ns_drumless.CopyFrom(ns)
     #   del ns_drumless.notes[:]
     #   ns_drumless.notes.extend([note for note in ns.notes if not note.is_drum])
-      return ns_drumless
+    #   return ns_drumless
 
-    est_ns_drumless = remove_drums(est_ns)
-    ref_ns_drumless = remove_drums(ref_ns)
+    # est_ns_drumless = remove_drums(est_ns)
+    # ref_ns_drumless = remove_drums(ref_ns)
 
-    # Whether or not there are separate tracks, compute metrics for the full
-    # NoteSequence minus drums.
-    est_tracks = [est_ns_drumless]
-    ref_tracks = [ref_ns_drumless]
+    est_tracks = [est_ns]
+    ref_tracks = [ref_ns]
     use_track_offsets = [False]
     use_track_velocities = [False]
     track_instrument_names = ['']
 
+    # this part calculates instrument-agnostic onset F1 score
+    # it is the same as: https://github.com/magenta/mt3/blob/main/mt3/metrics.py#L255
     for est_ns, ref_ns, use_offsets, use_velocities, instrument_name in zip(
         est_tracks, ref_tracks, use_track_offsets, use_track_velocities,
         track_instrument_names):
@@ -270,6 +113,9 @@ def mt3_program_aware_note_scores(
     ref_inst_to_notes_mapping = {}
     est_inst_to_notes_mapping = {}
 
+    # this part calculates multi-instrument onset F1 score
+    # based on: https://github.com/magenta/mt3/blob/main/mt3/metrics.py#L36
+    
     # following MT3, this will group notes under the same instrument program, determined by the granularity
     for inst in ref_mid.instruments:
         cur_ref_program = get_granular_program(inst.program, inst.is_drum, granularity_type)
@@ -285,6 +131,7 @@ def mt3_program_aware_note_scores(
         else:
             est_inst_to_notes_mapping[(cur_est_program, inst.is_drum)] = [note for note in inst.notes]
     
+    # this part is based on: https://github.com/magenta/mt3/blob/main/mt3/metrics.py#L82
     program_and_is_drum_tuples = set(ref_inst_to_notes_mapping.keys()) | set(est_inst_to_notes_mapping.keys())
     drum_precision_sum = 0.0
     drum_precision_count = 0
@@ -325,6 +172,11 @@ def mt3_program_aware_note_scores(
         precision, recall, f_measure, unused_avg_overlap_ratio = (
             mir_eval.transcription.precision_recall_f1_overlap(**args))
         
+        # print(f"program={program} is_drum={is_drum} est={est_pitches.shape[0]} ref={ref_pitches.shape[0]}")
+        # print(f"precision={precision} recall={recall} f_measure={f_measure}")
+        # print(f"est_intervals={len(est_intervals)} ref_intervals={len(ref_intervals)}")
+        # print("======")
+        
         if granularity_type == "midi_class":
             if is_drum:
                 program_f1[-1] = f_measure
@@ -347,6 +199,9 @@ def mt3_program_aware_note_scores(
     recall_sum = drum_recall_sum + nondrum_recall_sum
     recall_count = drum_recall_count + nondrum_recall_count
 
+    # print(f"precision_sum={precision_sum} precision_count={precision_count}")
+    # print(f"recall_sum={recall_sum} recall_count={recall_count}")
+
     precision = (precision_sum / precision_count) if precision_count else 0
     recall = (recall_sum / recall_count) if recall_count else 0
     f_measure = mir_eval.util.f_measure(precision, recall)
@@ -363,7 +218,6 @@ def mt3_program_aware_note_scores(
                         if nondrum_recall_count else 0)
     nondrum_f_measure = mir_eval.util.f_measure(nondrum_precision, nondrum_recall)
 
-    # print("v1", program_f1)
     res.update({
         f'Onset + program precision ({granularity_type})': precision,
         f'Onset + program recall ({granularity_type})': recall,
@@ -380,9 +234,6 @@ def mt3_program_aware_note_scores(
         "F1 by program": program_f1
     })
     return res
-    # })
-    # print("{:.4} {:.4} {:.4}".format(f_measure, drum_f_measure, nondrum_f_measure))
-    # return f_measure
 
 
 def loop_transcription_eval(ref_mid, est_mid):
@@ -426,47 +277,40 @@ from tqdm import tqdm
 
 # Slakh
 # fname = "slakh_recon_randomorder"
+# fname = "slakh_recon_randomorder_pix2seq"
+# fname = "slakh_recon_randomorder_lite_enc"
+# fname = "slakh_recon_randomorder_gru_bs=8"
+# fname = "slakh_recon_randomorder_lstm_2"
 # fname = "slakh_recon_norm_v2_0.5964"
 fname = "slakh_baseline"
+# fname = "slakh_recon_xl2_context=2048"
+# fname = "slakh_recon_xl2_inst_context=2048"
+# fname = "slakh_recon_xl2_inst"
+# fname = "slakh_recon_xl2_context=512"
+# fname = "slakh_mt3_hidden_state_eval"
 
 dir = sorted(glob.glob(f"{fname}/*/mix.mid"))    # TODO: this is just for fast evaluation
+dir = dir[:1]
 dir2 = [k.replace(f"{fname}/", "/data/slakh2100_flac_redux/test/").replace("/mix.mid", "/all_src_v2.mid") for k in dir]
 fnames = zip(dir2, dir)
 
 
 def func(item):
     fname1, fname2 = item
-    # print(fname1, fname2)
 
-    # 1. Perceiver style onset F1
-    # return compute_transcription_metrics(fname1, fname2)
-
-    # 2. Multi-instrument F1
-    ref_mid = pretty_midi.PrettyMIDI(fname1)
-    est_mid = pretty_midi.PrettyMIDI(fname2)
     results = {}
     for granularity in ["flat", "full", "midi_class"]:
+        # print("\ngranularity:", granularity)
         dic = mt3_program_aware_note_scores(
             fname1, fname2, granularity
         )
         results.update(dic)
-    
-    # 3. Multi-instrument F1 V2
-    # results = {}
-    # for granularity in ["flat", "midi_class", "full"]:
-    #     dic = mt3_program_aware_note_scores_v2(
-    #         fname1, fname2, granularity, offset=False       # NOTE: offset should be true in MT3
-    #     )
-    #     results.update(dic)
     
     return results
 
 
 pbar = tqdm(total=len(dir))
 scores = collections.defaultdict(list)
-# for fname in fnames:
-#     func(fname)
-#     break
 with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
     # Start the load operations and mark each future with its URL
     future_to_fname = {executor.submit(func, fname): fname for fname in fnames}
@@ -510,10 +354,10 @@ d = {
     11: "Synth Pad",
     12: "Synth Effects",
 }
-print(program_f1_dict)
-program_f1_dict = {k: np.mean(np.array(v)) for k, v in program_f1_dict.items()}
-for key in d:
-    if key == -1:
-        print("{}: {:.4}".format(d[key], program_f1_dict[key]))
-    elif key * 8 in program_f1_dict:
-        print("{}: {:.4}".format(d[key], program_f1_dict[key * 8]))
+# print(program_f1_dict)
+# program_f1_dict = {k: np.mean(np.array(v)) for k, v in program_f1_dict.items()}
+# for key in d:
+#     if key == -1:
+#         print("{}: {:.4}".format(d[key], program_f1_dict[key]))
+#     elif key * 8 in program_f1_dict:
+#         print("{}: {:.4}".format(d[key], program_f1_dict[key * 8]))
