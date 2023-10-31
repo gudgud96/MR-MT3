4820c02311deb8c6e87d3aa5a14be684f79606f0
Kin Wai Cheuk <u3500684@connect.hku.hk>
2023-10-31 15:45:47 +0900

    added hydra config manager

--- mt3_net.py
+++ mt3_net.py
@@ -26,18 +26,17 @@ import copy
 import argparse
 import mlflow
 import datetime
+import hydra
 
 
 class MT3Net(pl.LightningModule):
 
-    def __init__(self, config, model_config_path='config/mt3_config.json', result_dir='./results'):
+    def __init__(self, config, result_dir='./results'):
         super().__init__()
         self.config = config
         self.result_dir = result_dir
-        with open(model_config_path) as f:
-            config_dict = json.load(f)
-        config = T5Config.from_dict(config_dict)
-        self.model: nn.Module = T5ForConditionalGeneration(config)
+        T5config = T5Config.from_dict(OmegaConf.to_container(self.config.model))
+        self.model: nn.Module = T5ForConditionalGeneration(T5config)
 
     def forward(self, *args, **kwargs):
         return self.model.forward(*args, **kwargs)
@@ -78,61 +77,50 @@ class MT3Net(pl.LightningModule):
         # return AdamW(self.model.parameters(), self.config.lr)
 
     def train_dataloader(self):
-        dataset = SlakhDataset(root_dir='/data/slakh2100_flac_redux/train/', mel_length=self.config.mel_length, event_length=self.config.event_length, **self.config.data.config)
+        dataset = SlakhDataset(**self.config.data.train)
         train_loader = DataLoader(
             dataset, 
-            batch_size=1, 
-            num_workers=12, 
+            **self.config.dataloader.train,
             collate_fn=collate_fn
         )
         return train_loader
 
     def val_dataloader(self):
-        dataset = SlakhDataset(root_dir='/data/slakh2100_flac_redux/validation/', mel_length=self.config.mel_length, event_length=self.config.event_length, **self.config.data.config)
+        dataset = SlakhDataset(**self.config.data.val)
         val_loader = DataLoader(
             dataset, 
-            batch_size=1, 
-            num_workers=12, 
+            **self.config.dataloader.val,
             collate_fn=collate_fn
         )
         return val_loader
 
+@hydra.main(config_path="config", config_name="config")
+# def main(config, model_config, result_dir, mode, path):
+def main(cfg):
+    model = MT3Net(cfg)
+    logger = TensorBoardLogger(save_dir='.',
+                               name='dummy_run')
+    # TODO: use config file name 
+    # https://stackoverflow.com/a/70070004/9793316
 
-def main(config, model_config, result_dir, mode, path):
-    model = MT3Net(config, model_config, result_dir)
-    logger = TensorBoardLogger(save_dir='/'.join(result_dir.split('/')[:-1]),
-                               name=result_dir.split('/')[-1])
-
-    num_epochs = int(config['num_epochs'])
+    num_epochs = int(cfg.num_epochs)
     lr_monitor = LearningRateMonitor(logging_interval='step')
 
-    checkpoint_callback = ModelCheckpoint(
-        monitor='val_loss', 
-        mode='min', 
-        save_last=True, 
-        save_top_k=5, 
-        save_weights_only=False, 
-        filename='{epoch}-{step}-{val_loss:.4f}'
-    )
+    checkpoint_callback = ModelCheckpoint(**cfg.modelcheckpoint)
     tqdm_callback = TQDMProgressBar(refresh_rate=1)
 
-    if mode == "train":
+    if cfg.mode == "train":
         trainer = pl.Trainer(
             logger=logger,
-            default_root_dir=os.path.join(os.getcwd(), 'logs'),
+            # default_root_dir=os.path.join(os.getcwd(), 'logs'),
+            default_root_dir='.',
             callbacks=[lr_monitor, checkpoint_callback, tqdm_callback],
-            precision=32,
-            max_epochs=num_epochs,
-            accelerator='gpu',
-            accumulate_grad_batches=config.grad_accum,
-            num_sanity_val_steps=2,
-            log_every_n_steps=645,
-            strategy="ddp_find_unused_parameters_false",
-            devices=2
+            **cfg.trainer
         )
         trainer.fit(model)
 
     else:
+        # TODO: still haven't apply hydra to this part yet
         model = MT3Net.load_from_checkpoint(
             path,
             config=config,
@@ -150,22 +138,22 @@ def main(config, model_config, result_dir, mode, path):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', default="train")      # option that takes a value
-    parser.add_argument('--path')      # option that takes a value
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--mode', default="train")      # option that takes a value
+    # parser.add_argument('--path')      # option that takes a value
+    # args = parser.parse_args()
 
-    conf_file = 'config/config.yaml'
-    model_config = 'config/mt3_config.json'
-    print(f'Config {conf_file}')
-    conf = OmegaConf.load(conf_file)
+    # conf_file = 'config/config.yaml'
+    # model_config = 'config/mt3_config.json'
+    # print(f'Config {conf_file}')
+    # conf = OmegaConf.load(conf_file)
     
-    result_dir = ""
-    if args.mode == "train":
-        datetime_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        result_dir = f"logs/results_norm_randomorder_{datetime_str}"
-        print('Creating: ', result_dir)
-        os.makedirs(result_dir, exist_ok=False)
-        shutil.copy(conf_file, f'{result_dir}/config.yaml')
+    # result_dir = ""
+    # if args.mode == "train":
+    #     datetime_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    #     result_dir = f"logs/results_norm_randomorder_{datetime_str}"
+    #     print('Creating: ', result_dir)
+    #     os.makedirs(result_dir, exist_ok=False)
+    #     shutil.copy(conf_file, f'{result_dir}/config.yaml')
     
-    main(conf, model_config, result_dir, args.mode, args.path)
+    main()
