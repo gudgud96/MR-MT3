0a6d44c6c9f44764e5e48e0319d236ed9a73f337
gudgud96 <tanh0207@e.ntu.edu.sg>
2023-11-09 10:36:57 +0800

    [feat] add test.py

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import os
 from tqdm import tqdm
 import librosa
 import hydra
+import numpy as np
 from evaluate import evaluate_main
 
 
@@ -45,32 +46,59 @@ def main(cfg):
 
     exp_tag_name = cfg.eval.exp_tag_name
 
+    invalid_events_perc, dropped_events_perc, adjacent_program_events_perc = 0, 0, 0
+    est_exc_dict = {}
+
     for fname in tqdm(dir):
         audio = func(fname)
         
         if cfg.eval.eval_dataset == "Slakh":
             name = fname.split("/")[-2]
             outpath = os.path.join(exp_tag_name, name, "mix.mid")
-        elif cfg.eval.eval_dataset == "ComMU":
+        elif cfg.eval.eval_dataset == "ComMU" or cfg.eval.eval_dataset == "NSynth":
             name = fname.split("/")[-1]
             outpath = os.path.join(exp_tag_name,  name.replace(".wav", ".mid"))
         else:
             raise ValueError("Invalid dataset name.")
 
-        handler.inference(
+        ret_dict = handler.inference(
             audio, 
             fname, 
             outpath=outpath,
             batch_size=8,        # changing this might affect results of sequential-inference models (e.g. XL).
             max_length=256
         )
+
+        invalid_events_perc += ret_dict["invalid_events_perc"]
+        dropped_events_perc += ret_dict["dropped_events_perc"]
+        adjacent_program_events_perc += ret_dict["adjacent_program_events_perc"]
+        for key in ret_dict["est_exc_dict"]:
+            if key not in est_exc_dict:
+                est_exc_dict[key] = ret_dict["est_exc_dict"][key]
+            else:
+                est_exc_dict[key] += ret_dict["est_exc_dict"][key]
+    
+    print("==========================================")
+    print("|          Invalid events check          |")
+    print("==========================================")
+    print("Invalid events percentage:", invalid_events_perc / len(dir))
+    print("Dropped events percentage:", dropped_events_perc / len(dir))
+    print("Adjacent program events percentage:", adjacent_program_events_perc / len(dir))
+    print()
+    print("Invalid events breakdown:")
+    for key in est_exc_dict:
+        print('"' + key + '" - ', est_exc_dict[key])
+    print("==========================================")
+
     
     print("Evaluating...")
     current_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+
+    ground_truth_midi_dir = cfg.eval.midi_dir if cfg.eval.midi_dir is not None else cfg.dataset.test.root_dir
     evaluate_main(
         dataset_name=cfg.eval.eval_dataset,
         test_midi_dir=os.path.join(current_dir, cfg.eval.exp_tag_name),
-        ground_truth_midi_dir=cfg.dataset.test.root_dir,
+        ground_truth_midi_dir=ground_truth_midi_dir,
     )
 
 
