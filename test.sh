b30749ff4818b680b1cbec52cb21d3fe068fb0c0
gudgud96 <tanh0207@e.ntu.edu.sg>
2023-11-11 15:46:36 +0800

    [chore] add test.sh

--- test.sh
+++ test.sh
@@ -1,21 +1,12 @@
 # ComMU
-# python3 test.py \
-#     --config-path "config" \
-#     --config-name "config_commu" \
-#     path="../../../pretrained/commu_mt3.pt" \
-#     eval.eval_dataset="ComMU" \
-#     eval.exp_tag_name="commu_mt3" \
-#     eval.audio_dir="/data/datasets/ComMU/dataset_processed/commu_audio_v2/test/*.wav" \
-#     hydra/job_logging=disabled \
-
-# python3 test.py \
-#     --config-path "config" \
-#     --config-name "config_commu" \
-#     path="../../../pretrained/commu_mt3.pt" \
-#     eval.eval_dataset="Slakh" \
-#     eval.exp_tag_name="commu_mt3_on_slakh" \
-#     eval.audio_dir="/data/slakh2100_flac_redux/test/*/mix_16k.wav" \
-#     hydra/job_logging=disabled \
+python3 test.py \
+    --config-path "config" \
+    --config-name "config_commu" \
+    path="../../../pretrained/commu_mt3.pt" \
+    eval.eval_dataset="ComMU" \
+    eval.exp_tag_name="commu_mt3" \
+    eval.audio_dir="/data/datasets/ComMU/dataset_processed/commu_audio_v2/test/*.wav" \
+    hydra/job_logging=disabled \
 
 
 # Slakh MT3 baseline
@@ -29,14 +20,14 @@
     
 
 # NSynth
-python3 test.py \
-    path="../../../pretrained/mt3.pth" \
-    eval.eval_dataset="NSynth" \
-    eval.exp_tag_name="slakh_mt3_on_nsynth" \
-    eval.audio_dir="/data/nsynth-valid/audio/*.wav" \
-    eval.midi_dir="/data/nsynth-valid/midi/" \
-    hydra/job_logging=disabled \
-    # eval.is_sanity_check=True \
+# This is to compare between the performance of MT3 trained on Slakh / ComMU, on NSynth dataset
+# python3 test.py \
+#     path="../../../pretrained/mt3.pth" \
+#     eval.eval_dataset="NSynth" \
+#     eval.exp_tag_name="slakh_mt3_on_nsynth" \
+#     eval.audio_dir="/data/nsynth-valid/audio/*.wav" \
+#     eval.midi_dir="/data/nsynth-valid/midi/" \
+#     hydra/job_logging=disabled \
 
 # python3 test.py \
 #     --config-path "config" \
@@ -47,4 +38,3 @@ python3 test.py \
 #     eval.audio_dir="/data/nsynth-valid/audio/*.wav" \
 #     eval.midi_dir="/data/nsynth-valid/midi/" \
 #     hydra/job_logging=disabled \
-#     # eval.is_sanity_check=True \
