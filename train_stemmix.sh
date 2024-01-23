f352c5e12c3ca5f2c83719c15680d1a0c8c9a021
gudgud96 <tanh0207@e.ntu.edu.sg>
2024-01-23 23:48:11 +0800

    [chore] use multi-gpu for stemmix

--- train_stemmix.sh
+++ train_stemmix.sh
@@ -1,7 +1,7 @@
 HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
     --config-path="config" \
     --config-name="config_slakh_f1_0.65" \
-    devices=[1] \
+    devices=[0,1] \
     hydra/job_logging=disabled \
     model="MT3Net" \
     dataset="SlakhStemMix" \
