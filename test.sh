1a72531de5cad2f04885daa3b37c78f8cde889c6
gudgud96 <tanh0207@e.ntu.edu.sg>
2024-01-29 12:55:38 +0800

    [chore] resolve stash conflicts

--- test.sh
+++ test.sh
@@ -24,19 +24,40 @@
 #     eval.eval_first_n_examples=1 \
 #     eval.contiguous_inference=False \
 
-python3 test.py \
+# MT3 pretrained, but load weights for segmem
+# path="../../../pretrained/mt3.pth" \
+# path="../../../outputs/2024-01-28/15-38-25/MT3NetSegMemV2WithPrevFineTune_SlakhPrev/version_0/checkpoints/last.ckpt" \
+
+CUDA_VISIBLE_DEVICES=0 python3 test.py \
     --config-dir="config" \
-    --config-name="config_slakh_f1_0.65" \
-    model="MT3Net" \
-    path="../../../outputs/2023-11-24/11-15-55/MT3Net_Slakh/version_0/checkpoints/last.ckpt" \
+    --config-name="config_slakh_segmem" \
+    model="MT3NetSegMemV2WithPrev" \
+    path="../../../pretrained/exp_segmemV2_prev_context\=64_prevaug_frame\=8.ckpt" \
     eval.eval_dataset="Slakh" \
     eval.exp_tag_name="slakh_mt3_official" \
     eval.audio_dir="/data/slakh2100_flac_redux/test/*/mix_16k.wav" \
     hydra/job_logging=disabled \
     eval.is_sanity_check=True   \
-    eval.eval_first_n_examples=1 \
-    eval.contiguous_inference=False \
+    eval.contiguous_inference=True \
     eval.use_tf_spectral_ops=False \
+    model_segmem_length=64 \
+    split_frame_length=2000 \
+    eval.eval_first_n_examples=1 \
+    +eval.load_weights_strict=False \
+
+# python3 test.py \
+#     --config-dir="config" \
+#     --config-name="config_slakh_f1_0.65" \
+#     model="MT3Net" \
+#     path="../../../outputs/2023-11-24/11-15-55/MT3Net_Slakh/version_0/checkpoints/epoch\=379-step\=245100-val_loss\=1.3445.ckpt" \
+#     eval.eval_dataset="Slakh" \
+#     eval.exp_tag_name="slakh_mt3_official" \
+#     eval.audio_dir="/data/slakh2100_flac_redux/test/*/mix_16k.wav" \
+#     hydra/job_logging=disabled \
+#     eval.is_sanity_check=True   \
+#     eval.contiguous_inference=False \
+#     eval.use_tf_spectral_ops=False \
+    # eval.eval_first_n_examples=1 \
 
 # python3 test.py \
 #     --config-dir="config" \
--- train.sh
+++ train.sh
@@ -35,4 +35,99 @@ HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
 #     devices=[0,1] \
 #     hydra/job_logging=disabled \
 #     model="MT3Net" \
+<<<<<<< Updated upstream
 #     dataset="Slakh" \
+=======
+#     dataset="Slakh" \
+#     split_frame_length=2000 \
+
+# train segmem with MT3 split_frame + prev_frame
+# HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
+#     --config-path="config" \
+#     --config-name="config_slakh_segmem" \
+#     devices=[0,1] \
+#     hydra/job_logging=disabled \
+#     model="MT3NetSegMemV2WithPrev" \
+#     dataset="SlakhPrev" \
+#     split_frame_length=2000 \
+#     model_segmem_length=0 \
+#     trainer.check_val_every_n_epoch=1 \
+
+# HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
+#     --config-path="config" \
+#     --config-name="config_slakh_segmem" \
+#     devices=[0,1] \
+#     hydra/job_logging=disabled \
+#     model="MT3NetSegMemV2WithPrev" \
+#     dataset="SlakhPrev" \
+#     split_frame_length=2000 \
+#     model_segmem_length=64 \
+#     trainer.check_val_every_n_epoch=20 \
+
+# HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
+#     --config-path="config" \
+#     --config-name="config_slakh_segmem" \
+#     devices=[0,1] \
+#     hydra/job_logging=disabled \
+#     model="MT3NetSegMemV2WithPrev" \
+#     dataset="SlakhPrevAugment" \
+#     split_frame_length=2000 \
+#     model_segmem_length=64 \
+#     dataset_prev_augment_frames=8 \
+#     trainer.check_val_every_n_epoch=20 \
+#     path="../../../outputs/2024-01-12/15-03-59/MT3NetSegMemV2WithPrev_SlakhPrevAugment/version_0/checkpoints/last.ckpt" \
+
+# HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
+#     --config-path="config" \
+#     --config-name="config_slakh_segmem" \
+#     devices=[0,1] \
+#     hydra/job_logging=disabled \
+#     model="MT3NetSegMemV2WithPrev" \
+#     dataset="SlakhPrevAugment" \
+#     dataset_use_tf_spectral_ops=False \
+#     split_frame_length=2000 \
+#     model_segmem_length=64 \
+#     dataset_prev_augment_frames=3 \
+#     trainer.check_val_every_n_epoch=20 \
+#     path="../../../outputs/2024-01-21/10-33-08/MT3NetSegMemV2WithPrev_SlakhPrevAugment/version_0/checkpoints/last.ckpt" \
+
+
+HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
+    --config-path="config" \
+    --config-name="config_slakh_segmem" \
+    devices=[0,1] \
+    hydra/job_logging=disabled \
+    model="MT3NetSegMemV2WithPrevFineTune" \
+    dataset="SlakhPrev" \
+    dataset_use_tf_spectral_ops=True \
+    dataset_is_randomize_tokens=False \
+    split_frame_length=2000 \
+    model_segmem_length=0 \
+    trainer.check_val_every_n_epoch=20 \
+    path="../../../pretrained/mt3.pth" \
+    optim.lr=1e-5 \
+    # dataset_prev_augment_frames=3 \
+
+
+# HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
+#     --config-path="config" \
+#     --config-name="config_slakh_f1_0.65" \
+#     devices=[1] \
+#     hydra/job_logging=disabled \
+#     model="MT3Net" \
+#     dataset="SlakhStemMix" \
+#     dataset_use_tf_spectral_ops=False \
+#     split_frame_length=2000 \
+#     trainer.check_val_every_n_epoch=20 \
+
+# HYDRA_FULL_ERROR=1 OMP_NUM_THREADS=1 python3 train.py \
+#     --config-path="config" \
+#     --config-name="config_slakh_segmem" \
+#     devices=[0,1] \
+#     hydra/job_logging=disabled \
+#     model="MT3NetSegMemV3" \
+#     dataset="SlakhPrev" \
+#     split_frame_length=2000 \
+#     model_segmem_length=64 \
+#     trainer.check_val_every_n_epoch=20 \
+>>>>>>> Stashed changes
